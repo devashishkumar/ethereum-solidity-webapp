3725ff30db359e619e3cfcbd24ff421ea1794518
devashishkumar <dev.ashishkumar0512@gmail.com>
2022-01-26 15:02:41 +0530

    verify smart contract deployment

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -11,12 +11,13 @@ beforeEach(async () => {
     accounts = await web.eth.getAccounts();
     // console.log(compiledCode.interface, compiledCode.byteCode);
     inbox = await new web.eth.Contract(compiledCode.interface)
-    .deploy({data: compiledCode.byteCode, arguments: ['Hello']})
-    .send({from: accounts[0], gas: '1000000'})
+        .deploy({ data: compiledCode.byteCode, arguments: ['Hello'] })
+        .send({ from: accounts[0], gas: '1000000' })
 });
 
 describe('Inbox', () => {
     it('deploy contract', () => {
-        console.log(inbox, '18');
+        console.log(inbox.options.address);
+        assert.ok(inbox.options.address);
     });
 });
