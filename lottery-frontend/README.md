93476807896191c5a8f87518c050664e26ea03a4
devashishkumar <dev.ashishkumar0512@gmail.com>
2022-01-30 16:41:22 +0530

    use all the methods written in smart contracts from angular ui

--- lottery-frontend/README.md
+++ lottery-frontend/README.md
@@ -1,14 +1,12 @@
-# Lottery Frontend
-
-## Development server
-
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+## Lottery Frontend
 
 
 #### How to use this blockchain app
 
-- In project root and lottery-frontend directory execute command 'npm install'
-- In project root directory execute command 'npm run test'. lottery test cases will execute.
+- In project root and lottery-frontend directory execute command `npm install`
+- In project root directory execute command `npm run test`. lottery test cases will execute.
 - Copy abi code and user address from lottery.test.js test file
 - Paste abi code and user address in app.component.ts
+- execute `npm serve` command from lottery-frontend directory
 - Open `http://localhost:4200` in browser
+- All the methods written in smart contracts are accessible from angular UI
--- lottery-frontend/src/app/app.component.ts
+++ lottery-frontend/src/app/app.component.ts
@@ -8,13 +8,18 @@ declare var Web3: any;
 })
 export class AppComponent implements OnInit {
   title = 'lottery-frontend';
-  serverInst = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
-  // serverInst = new Web3(window['web3'].currentProvider);
+  web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
+  // web3 = new Web3(window['web3'].currentProvider);
   blockchainAccounts: any = [];
   address = '0x51A54343D704E6e8AD072C14C52A7ADE4cb34937';
   abi: any = [{ "inputs": [], "stateMutability": "nonpayable", "type": "constructor", "signature": "constructor" }, { "inputs": [], "name": "enter", "outputs": [], "stateMutability": "payable", "type": "function", "payable": true, "signature": "0xe97dcb62" }, { "inputs": [], "name": "getPlayers", "outputs": [{ "internalType": "address[]", "name": "", "type": "address[]" }], "stateMutability": "view", "type": "function", "constant": true, "signature": "0x8b5b9ccc" }, { "inputs": [], "name": "manager", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function", "constant": true, "signature": "0x481c6a75" }, { "inputs": [], "name": "pickWinner", "outputs": [], "stateMutability": "nonpayable", "type": "function", "signature": "0x5d495aea" }, { "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "name": "players", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function", "constant": true, "signature": "0xf71d96cb" }];
   lotteryContract: any;
   manager = '';
+  players: any = [];
+  balance = '';
+  formContent = {
+    amount: 0
+  };
 
   ngOnInit(): void {
     this.getAccounts();
@@ -24,14 +29,12 @@ export class AppComponent implements OnInit {
    * get blockchain connected accounts
    */
   getAccounts() {
-    this.serverInst.eth.getAccounts().then((response: any) => {
-      console.log(response);
+    this.web3.eth.getAccounts().then((response: any) => {
       if (response && response.length) {
         this.blockchainAccounts = response;
         this.getContract();
       }
     }, (error: any) => {
-      console.log(error);
     });
   }
 
@@ -39,8 +42,10 @@ export class AppComponent implements OnInit {
    * contract instance
    */
   getContract() {
-    this.lotteryContract = new this.serverInst.eth.Contract(this.abi, this.address);
+    this.lotteryContract = new this.web3.eth.Contract(this.abi, this.address);
     this.getManager()
+    this.getPlayers();
+    this.getBalance();
   }
 
   /**
@@ -48,6 +53,61 @@ export class AppComponent implements OnInit {
    */
   async getManager() {
     this.manager = await this.lotteryContract.methods.manager().call();
+  }
+
+  /**
+   * get players who paid lottery amount
+   */
+  async getPlayers() {
+    this.players = await this.lotteryContract.methods.getPlayers().call();
+  }
+
+  /**
+   * get user balance
+   */
+  async getBalance() {
+    this.balance = await this.web3.eth.getBalance(this.lotteryContract.options.address);
+    this.balance = this.web3.utils.fromWei(this.balance, 'ether');
+  }
+
+  /**
+   * get form value changes
+   * @param data number
+   */
+  valueChange(data: any) {
+  }
+
+  /**
+   * submit form
+   */
+  submitForm() {
+    if (this.formContent.amount && this.formContent.amount > 0) {
+      this.addPlayer();
+    }
+  }
+
+  /**
+   * add player to the lottery
+   */
+  async addPlayer() {
+    await this.lotteryContract.methods.enter().send({
+      from: this.blockchainAccounts[0],
+      value: this.web3.utils.toWei(String(this.formContent.amount), 'ether')
+    });
+    this.getBalance();
+  }
+
+  pickWinner() {
+    this.findWinner();
+  }
+
+  /**
+   * find winner
+   */
+  async findWinner() {
+    await this.web3.methods.pickWinner().send({
+      from: this.blockchainAccounts[0]
+    })
   }
 
 }
